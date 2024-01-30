390d27e992fd720ffd6872f5cb90702ce37ac346
Thomas <133188011+nekree444@users.noreply.github.com>
2024-01-29 22:48:06 -0500

    Added pipeline files

---
+++ mage-zoomcamp/pipeline-files/hw_taxi_data_exporter_partitioned.py
@@ -0,0 +1,28 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/nomadic-pipe-407907-4e774cfc4e3b.json"
+
+bucket_name = 'thomasm-de-zoomcamp-mage'
+project_id = 'nomadic-pipe-407907'
+
+table_name = "green_taxi_data"
+
+root_path = f'{bucket_name}/mage-hw/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['lpep_pickup_date'],
+        filesystem=gcs
+    )
